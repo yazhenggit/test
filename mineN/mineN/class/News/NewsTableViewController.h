d23dd58dcfc61f0b9a1432b5313917484eb132ca
王亚征 <yazheng@yazhengdeMacBook-Pro.local>
2015-11-06 19:41:25 +0800

    添加 AFN 框架

---
+++ mineN/mineN/class/News/NewsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  NewsTableViewController.h
+//  mineN
+//
+//  Created by 王亚征 on 15/11/6.
+//  Copyright © 2015年 yazheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewsTableViewController : UITableViewController
+
+@end
